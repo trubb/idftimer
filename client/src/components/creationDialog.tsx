aaed4a18707b8766c820a347f21aee72bb31169e
trubb <trubb@example.com>
2025-02-11 22:57:21 +0100

    Are we less janky NOW?!

--- client/src/components/creationDialog.tsx
+++ client/src/components/creationDialog.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useContext} from "react";
-import { flushSync } from "react-dom";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
